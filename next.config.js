e5de8c95fd18eb7e3f757a6eaccac3b5a6fdb056
Daniel Dawson-Brown <danielddb@hotmail.co.uk>
2019-10-17 15:47:59 +0100

    updated installed dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-      test: /\.(woff(2)?)(\?[a-z0-9=&.]+)?$/,
-      use: 'base64-inline-loader?limit=1000&name=[name].[ext]'
-    });
-
-    return config;
-  }
-};
+module.exports = {};
